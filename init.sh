26f8c5fb5613578f5b6f2f47ffdca135c39f65a6
Alin Florin <alin@qoffice.ro>
2023-02-09 23:57:41 +0200

    -

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-FILE=/app/src/index.ts
+FILE=/app/app/index.ts
 if test -f "$FILE"; then
   echo "Code already exists"
 else
   echo "Code doesn't exist. Copying..."
   cp -R /template/* /app
+  chmod -R 777 /app
 fi
 cd /app
 npm i
